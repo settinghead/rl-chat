b419f6073229ffca8dfced0d0e024b879b746120
Xiyang Chen <settinghead@gmail.com>
2018-10-13 00:29:22 -0400

    more main code

--- play.py
+++ play.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
 from itertools import count
-from policy_net import Encoder, Decoder
+from encoder_decoder import Encoder, Decoder
 from environment import Environment
 
 tf.enable_eager_execution()
@@ -10,17 +10,44 @@ tf.enable_eager_execution()
 EPISODES = 100
 
 
+class Agent:
+    def __init__(self):
+        pass
+
+    def encoder_z(self, state):
+        pass
+
+
 def main():
     env = Environment()
+    agent = Agent()
 
     for episode in range(EPISODES):
 
         state = env.reset()
 
-        for c in count():
-            action = agent.calc(state)
+        acc_rewards = []
+        acc_actions = []
+
+        while True:
+            encoder_z = agent.encoder_z(state)
+            actions_with_probs = beam_search(encoder_z)
+            actions_dist = Categorical(actions_with_probs)
+
+            action = actions_dist.sample()
+
+            loss = get_loss(actions_dist)
+
             state, reward, done = env.step(action)
 
             # To mark boundaries between episodes
             if done:
                 reward = 0
+
+            acc_rewards.append(reward)
+            acc_actions.append(action)
+
+        # Update policy
+        if episode > 0 and episode % batch_size == 0:
+            # TODO
+            pass
