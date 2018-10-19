bf2b66e143898b0e7fa89a78ae639ec0fa46c343
Renyu LI <renyuli@Renyus-MacBook-Pro-2.local>
2018-10-18 23:42:06 -0400

    update seq2seq model and add new test file

--- encoder_decoder.py
+++ encoder_decoder.py
@@ -1,23 +1,29 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
 def gru(units):
-  if tf.test.is_gpu_available():
-    return tf.keras.layers.CuDNNGRU(units, 
+        if tf.test.is_gpu_available():
+            return tf.keras.layers.CuDNNGRU(units, 
+                                            return_sequences=True, 
+                                            return_state=True, 
+                                            recurrent_initializer='glorot_uniform')
+        else:
+            return tf.keras.layers.GRU(units, 
                                     return_sequences=True, 
                                     return_state=True, 
+                                    recurrent_activation='sigmoid', 
                                     recurrent_initializer='glorot_uniform')
-  else:
-    return tf.keras.layers.GRU(units, 
-                               return_sequences=True, 
-                               return_state=True, 
-                               recurrent_activation='sigmoid', 
-                               recurrent_initializer='glorot_uniform')
-
 
 class Encoder(tf.keras.Model):
-    def __init__(self, vocab_size, embedding_dim, enc_units, batch_sz):
+    def __init__(
+        self,
+        vocab_size,
+        embedding_dim,
+        enc_units,
+        batch_sz
+    ):
         super(Encoder, self).__init__()
         self.batch_sz = batch_sz
         self.enc_units = enc_units
@@ -33,7 +39,13 @@ class Encoder(tf.keras.Model):
         return tf.zeros((self.batch_sz, self.enc_units))
 
 class Decoder(tf.keras.Model):
-    def __init__(self, vocab_size, embedding_dim, dec_units, batch_sz):
+    def __init__(
+        self,
+        vocab_size,
+        embedding_dim,
+        dec_units,
+        batch_sz
+    ):
         super(Decoder, self).__init__()
         self.batch_sz = batch_sz
         self.dec_units = dec_units
@@ -44,9 +56,78 @@ class Decoder(tf.keras.Model):
         
     def call(self, x, hidden):
         x = self.embedding(x)
-        output, state = self.gru(x, initial_state=hidden)
+        output, state = self.gru(x, initial_state = hidden)
         x = self.fc(output)
         x = tf.reshape(x, [self.batch_sz, self.vocab_size])
         logits = tf.nn.softmax(x)
         return logits, state
+        
+class Seq2Seq(tf.keras.Model):
+    def __init__(
+        self,
+        vocab_inp_size,
+        vocab_tar_size,
+        embedding_dim,
+        enc_units,
+        batch_sz,
+        targ_lang
+    ):
 
+        super(Seq2Seq, self).__init__()
+        self.vocab_inp_size = vocab_inp_size
+        self.vocab_tar_size = vocab_tar_size
+        self.embedding_dim = embedding_dim
+        self.batch_sz = batch_sz
+        self.enc_units = enc_units
+        self.targ_lang = targ_lang
+        self.encoder = Encoder(vocab_inp_size, embedding_dim, enc_units, batch_sz)
+        self.decoder = Decoder(vocab_tar_size, embedding_dim, enc_units, batch_sz)
+        self.hidden = self.encoder.initialize_hidden_state()
+
+    def loss_function(self, real, pred):
+        mask = 1 - np.equal(real, 0)
+        loss_ = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=real, logits=pred) * mask
+        return tf.reduce_mean(loss_)
+
+    def call(self, inp, targ):
+        loss = 0
+        enc_output, enc_hidden = self.encoder(inp, self.hidden)
+        dec_hidden = enc_hidden
+        dec_input = tf.expand_dims([self.targ_lang.word2idx['<start>']] * self.batch_sz, 1)       
+        # Teacher forcing - feeding the target as the next input
+        for t in range(1, targ.shape[1]):
+            predictions, dec_hidden = self.decoder(dec_input, dec_hidden)
+            loss += self.loss_function(targ[:, t], predictions)
+            # using teacher forcing
+            dec_input = tf.expand_dims(targ[:, t], 1)
+
+        return loss
+
+
+def evaluate(model: Seq2Seq, eval_dataset):
+    """evaluate an epoch."""
+    total_loss = 0
+    for (batch, (inp, targ)) in enumerate(eval_dataset):
+        loss = model(inp, targ)
+        batch_loss = (loss / int(targ.shape[1]))
+        total_loss += batch_loss
+        if batch % 100 == 0:
+            print('batch {} eval loss {:.4f}'.format(batch, total_loss.numpy()))
+    
+    return total_loss
+
+def train(model: Seq2Seq, optimizer, train_dataset):
+    """training an epoch."""
+    total_loss = 0
+    for (batch, (inp, targ)) in enumerate(train_dataset):
+        with tf.GradientTape() as tape:
+            loss = model(inp, targ)
+            batch_loss = (loss / int(targ.shape[1]))
+            total_loss += batch_loss
+            variables = model.encoder.variables + model.decoder.variables
+            gradients = tape.gradient(loss, variables)
+            optimizer.apply_gradients(zip(gradients, variables))
+            if batch % 100 == 0:
+                print('batch {} training loss {:.4f}'.format(batch, total_loss.numpy()))
+
+    return total_loss
---
+++ train_seq2seq.py
@@ -0,0 +1,99 @@
+import tensorflow as tf
+import numpy as np
+import functools
+import encoder_decoder as encoder_decoder 
+import os
+import time
+from sklearn.model_selection import train_test_split
+
+
+class LanguageIndex():
+  def __init__(self, samples):
+    self.samples = samples
+    self.word2idx = {}
+    self.idx2word = {}
+    self.vocab = set()
+    self.create_index()
+    
+  def create_index(self):
+    for phrase in self.samples:
+      self.vocab.update(phrase.split(' '))
+    
+    self.vocab = sorted(self.vocab)
+    
+    self.word2idx['<pad>'] = 0
+    for index, word in enumerate(self.vocab):
+      self.word2idx[word] = index + 1
+    
+    for word, index in self.word2idx.items():
+      self.idx2word[index] = word
+      
+def max_length(tensor):
+    return max(len(t) for t in tensor)
+
+
+if __name__ == "__main__":
+    tf.enable_eager_execution()
+
+    from utils import random_utterance
+    samples = [random_utterance(4, 10) for _ in range(1000)]
+
+    BATCH_SIZE = 64
+    embedding_dim = 256
+    units = 1024
+  
+    inp_lang = LanguageIndex(samples)
+    targ_lang = LanguageIndex(samples)
+
+    vocab_inp_size = len(inp_lang.word2idx)
+    vocab_tar_size = len(targ_lang.word2idx)
+
+    optimizer = tf.train.AdamOptimizer()
+    EPOCHS = 10
+
+    checkpoint_dir = './training_checkpoints'
+    checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+    checkpoint = tf.train.Checkpoint(optimizer=optimizer)
+
+    input_tensor = [[inp_lang.word2idx[s] for s in sp.split(' ')] for sp in samples]
+    target_tensor = [[targ_lang.word2idx[s] for s in sp.split(' ')] for sp in samples]
+    # Calculate max_length of input and output tensor
+    # Here, we'll set those to the longest sentence in the dataset
+    max_length_inp, max_length_tar = max_length(input_tensor), max_length(target_tensor)
+
+    # Padding the input and output tensor to the maximum length
+    input_tensor = tf.keras.preprocessing.sequence.pad_sequences(input_tensor, 
+                                                                 maxlen=max_length_inp,
+                                                                 padding='post')
+    
+    target_tensor = tf.keras.preprocessing.sequence.pad_sequences(target_tensor, 
+                                                                  maxlen=max_length_tar, 
+                                                                  padding='post')
+        
+    # Creating training and validation sets using an 80-20 split
+    input_tensor_train, input_tensor_val, target_tensor_train, target_tensor_val = train_test_split(input_tensor, target_tensor, test_size=0.2)
+
+    BUFFER_SIZE = len(input_tensor_train)
+    dataset = tf.data.Dataset.from_tensor_slices((input_tensor_train, target_tensor_train)).shuffle(BUFFER_SIZE)
+    dataset = dataset.batch(BATCH_SIZE, drop_remainder=True)
+    EVAL_BUFFER_SIZE = len(input_tensor_val)
+    val_dataset = tf.data.Dataset.from_tensor_slices((input_tensor_val, target_tensor_val)).shuffle(EVAL_BUFFER_SIZE)
+    val_dataset = val_dataset.batch(BATCH_SIZE, drop_remainder=True)
+
+    N_BATCH = BUFFER_SIZE // BATCH_SIZE
+    
+    for epoch in range(EPOCHS):
+        start = time.time()
+        model = encoder_decoder.Seq2Seq(vocab_inp_size, vocab_tar_size, embedding_dim, units, BATCH_SIZE, targ_lang)
+        train_total_loss = encoder_decoder.train(model, optimizer, dataset)
+        eval_total_loss = encoder_decoder.evaluate(model, val_dataset)
+
+        # saving (checkpoint) the model every 2 epochs
+        if (epoch + 1) % 2 == 0:
+            checkpoint.save(file_prefix = checkpoint_prefix)
+        
+        print('Time taken for 1 epoch {} sec\n'.format(time.time() - start))
+
+
+            
+
