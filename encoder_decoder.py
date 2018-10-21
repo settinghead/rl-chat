b36a59d4bfde5c13fb39cf90f30705ae5fd3356f
Xiyang Chen <settinghead@gmail.com>
2018-10-20 20:33:54 -0400

    fix use_blstim

--- encoder_decoder.py
+++ encoder_decoder.py
@@ -6,34 +6,36 @@ import time
 BEGIN_TAG = '<GO>'
 END_TAG = '<EOS>'
 
+
 def gru(units):
     if tf.test.is_gpu_available():
-        return tf.keras.layers.CuDNNGRU(units, 
-                                        return_sequences=True, 
-                                        return_state=True, 
+        return tf.keras.layers.CuDNNGRU(units,
+                                        return_sequences=True,
+                                        return_state=True,
                                         recurrent_initializer='glorot_uniform')
     else:
-        return tf.keras.layers.GRU(units, 
-                                   return_sequences=True, 
-                                   return_state=True, 
-                                   recurrent_activation='sigmoid', 
+        return tf.keras.layers.GRU(units,
+                                   return_sequences=True,
+                                   return_state=True,
+                                   recurrent_activation='sigmoid',
                                    recurrent_initializer='glorot_uniform')
+
 
 def bilstm(units):
     if tf.test.is_gpu_available():
-        return tf.keras.layers.Bidirectional(tf.keras.layers.CuDNNLSTM(units, 
-                                                                       return_sequences=True, 
+        return tf.keras.layers.Bidirectional(tf.keras.layers.CuDNNLSTM(units,
+                                                                       return_sequences=True,
                                                                        return_state=True,
                                                                        dropout=0.25,
                                                                        recurrent_dropout=0.25))
     else:
-        return tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(units, 
-                                                                  return_sequences=True, 
+        return tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(units,
+                                                                  return_sequences=True,
                                                                   return_state=True,
                                                                   dropout=0.25,
-                                                                  recurrent_dropout=0.25, 
+                                                                  recurrent_dropout=0.25,
                                                                   recurrent_activation='sigmoid'))
-       
+
 
 class Encoder(tf.keras.Model):
     def __init__(
@@ -42,7 +44,7 @@ class Encoder(tf.keras.Model):
         embedding_dim,
         enc_units,
         batch_sz,
-        use_bilstm = False
+        use_bilstm=False
     ):
         super(Encoder, self).__init__()
         self.batch_sz = batch_sz
@@ -50,21 +52,24 @@ class Encoder(tf.keras.Model):
         self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
         self.use_bilstm = use_bilstm
         self.gru = gru(self.enc_units)
-        self.bilstm = bilstm(self.enc_units)
-        
+        if use_bilstm:
+            self.bilstm = bilstm(self.enc_units)
+
     def call(self, x, hidden):
         x = self.embedding(x)
         if self.use_bilstm:
-            output, forward_h, forward_c, backward_h, backward_c = self.bilstm(x, initial_state=hidden)
+            output, forward_h, forward_c, backward_h, backward_c = self.bilstm(
+                x, initial_state=hidden)
             state_h = tf.keras.layers.Concatenate([forward_h, backward_h])
             state_c = tf.keras.layers.Concatenate([forward_c, backward_c])
             state = [state_h, state_c]
         else:
-            output, state = self.gru(x, initial_state = hidden)        
+            output, state = self.gru(x, initial_state=hidden)
         return output, state
-    
+
     def initialize_hidden_state(self):
         return tf.zeros((self.batch_sz, self.enc_units))
+
 
 class Decoder(tf.keras.Model):
     def __init__(
@@ -73,7 +78,7 @@ class Decoder(tf.keras.Model):
         embedding_dim,
         dec_units,
         batch_sz,
-        use_bilstm = False
+        use_bilstm=False
     ):
         super(Decoder, self).__init__()
         self.batch_sz = batch_sz
@@ -82,23 +87,26 @@ class Decoder(tf.keras.Model):
         self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
         self.use_bilstm = use_bilstm
         self.gru = gru(self.dec_units)
-        self.bilstm = bilstm(self.dec_units)
+        if use_bilstm:
+            self.bilstm = bilstm(self.dec_units)
         self.fc = tf.keras.layers.Dense(vocab_size)
-        
+
     def call(self, x, hidden):
         x = self.embedding(x)
         if self.use_bilstm:
-            output, forward_h, forward_c, backward_h, backward_c  = self.bilstm(x, initial_state=hidden)
+            output, forward_h, forward_c, backward_h, backward_c = self.bilstm(
+                x, initial_state=hidden)
             state_h = tf.keras.layers.Concatenate([forward_h, backward_h])
             state_c = tf.keras.layers.Concatenate([forward_c, backward_c])
             state = [state_h, state_c]
         else:
-            output, state = self.gru(x, initial_state = hidden)
+            output, state = self.gru(x, initial_state=hidden)
         x = self.fc(output)
         x = tf.reshape(x, [self.batch_sz, self.vocab_size])
         logits = tf.nn.softmax(x)
         return logits, state
-        
+
+
 class Seq2Seq(tf.keras.Model):
     def __init__(
         self,
@@ -108,7 +116,7 @@ class Seq2Seq(tf.keras.Model):
         enc_units,
         batch_sz,
         targ_lang,
-        display_result = False
+        display_result=False
     ):
 
         super(Seq2Seq, self).__init__()
@@ -119,20 +127,24 @@ class Seq2Seq(tf.keras.Model):
         self.enc_units = enc_units
         self.targ_lang = targ_lang
         self.display_result = display_result
-        self.encoder = Encoder(vocab_inp_size, embedding_dim, enc_units, batch_sz)
-        self.decoder = Decoder(vocab_tar_size, embedding_dim, enc_units, batch_sz)
+        self.encoder = Encoder(
+            vocab_inp_size, embedding_dim, enc_units, batch_sz)
+        self.decoder = Decoder(
+            vocab_tar_size, embedding_dim, enc_units, batch_sz)
         self.hidden = self.encoder.initialize_hidden_state()
 
     def loss_function(self, real, pred):
         mask = 1 - np.equal(real, 0)
-        loss_ = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=real, logits=pred) * mask
+        loss_ = tf.nn.sparse_softmax_cross_entropy_with_logits(
+            labels=real, logits=pred) * mask
         return tf.reduce_mean(loss_)
 
     def call(self, inp, targ):
         loss = 0
         enc_output, enc_hidden = self.encoder(inp, self.hidden)
         dec_hidden = enc_hidden
-        dec_input = tf.expand_dims([self.targ_lang.word2idx[BEGIN_TAG]] * self.batch_sz, 1)
+        dec_input = tf.expand_dims(
+            [self.targ_lang.word2idx[BEGIN_TAG]] * self.batch_sz, 1)
         result = ''
         # Teacher forcing - feeding the target as the next input
         for t in range(1, targ.shape[1]):
@@ -160,8 +172,9 @@ def evaluate(model: Seq2Seq, eval_dataset):
         total_loss += batch_loss
         if batch % 100 == 0:
             print('batch {} eval loss {:.4f}'.format(batch, total_loss.numpy()))
-    
+
     return total_loss
+
 
 def train(model: Seq2Seq, optimizer, train_dataset):
     """training an epoch."""
@@ -175,6 +188,7 @@ def train(model: Seq2Seq, optimizer, train_dataset):
             gradients = tape.gradient(loss, variables)
             optimizer.apply_gradients(zip(gradients, variables))
             if batch % 100 == 0:
-                print('batch {} training loss {:.4f}'.format(batch, total_loss.numpy()))
+                print('batch {} training loss {:.4f}'.format(
+                    batch, total_loss.numpy()))
 
     return total_loss
