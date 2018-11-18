5436f1f1f5c70c656860590333928a055551258b
Xiyang Chen <settinghead@gmail.com>
2018-11-18 12:11:36 -0500

    copy bert code

--- bert_yo/run_squad.py
+++ bert_yo/run_squad.py
@@ -1,3 +1,18 @@
+# coding=utf-8
+# Copyright 2018 The Google AI Language Team Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Run BERT on SQuAD 1.1 and SQuAD 2.0."""
 
 from __future__ import absolute_import
 from __future__ import division
@@ -13,28 +28,25 @@ import optimization
 import tokenization
 import six
 import tensorflow as tf
-from utils import validate_flags_or_throw
 
 flags = tf.flags
 
 FLAGS = flags.FLAGS
 
-BERT_BASE_DIR = './pretrained/uncased_L-24_H-1024_A-16/'
-
-# Required parameters
+## Required parameters
 flags.DEFINE_string(
-    "bert_config_file", f'{BERT_BASE_DIR}/bert_config.json',
+    "bert_config_file", None,
     "The config json file corresponding to the pre-trained BERT model. "
     "This specifies the model architecture.")
 
-flags.DEFINE_string("vocab_file", f'{BERT_BASE_DIR}/vocab.txt',
+flags.DEFINE_string("vocab_file", None,
                     "The vocabulary file that the BERT model was trained on.")
 
 flags.DEFINE_string(
-    "output_dir", './checkpoints',
+    "output_dir", None,
     "The output directory where the model checkpoints will be written.")
 
-# Other parameters
+## Other parameters
 flags.DEFINE_string("train_file", None,
                     "SQuAD json for training. E.g., train-v1.1.json")
 
@@ -43,7 +55,7 @@ flags.DEFINE_string(
     "SQuAD json for predictions. E.g., dev-v1.1.json or test-v1.1.json")
 
 flags.DEFINE_string(
-    "init_checkpoint", f'{BERT_BASE_DIR}/bert_model.ckpt',
+    "init_checkpoint", None,
     "Initial checkpoint (usually from a pre-trained BERT model).")
 
 flags.DEFINE_bool(
@@ -76,8 +88,7 @@ flags.DEFINE_integer("train_batch_size", 32, "Total batch size for training.")
 flags.DEFINE_integer("predict_batch_size", 8,
                      "Total batch size for predictions.")
 
-flags.DEFINE_float("learning_rate", 5e-5,
-                   "The initial learning rate for Adam.")
+flags.DEFINE_float("learning_rate", 5e-5, "The initial learning rate for Adam.")
 
 flags.DEFINE_float("num_train_epochs", 3.0,
                    "Total number of training epochs to perform.")
@@ -105,6 +116,29 @@ flags.DEFINE_integer(
 
 flags.DEFINE_bool("use_tpu", False, "Whether to use TPU or GPU/CPU.")
 
+tf.flags.DEFINE_string(
+    "tpu_name", None,
+    "The Cloud TPU to use for training. This should be either the name "
+    "used when creating the Cloud TPU, or a grpc://ip.address.of.tpu:8470 "
+    "url.")
+
+tf.flags.DEFINE_string(
+    "tpu_zone", None,
+    "[Optional] GCE zone where the Cloud TPU is located in. If not "
+    "specified, we will attempt to automatically detect the GCE project from "
+    "metadata.")
+
+tf.flags.DEFINE_string(
+    "gcp_project", None,
+    "[Optional] Project name for the Cloud TPU-enabled project. If not "
+    "specified, we will attempt to automatically detect the GCE project from "
+    "metadata.")
+
+tf.flags.DEFINE_string("master", None, "[Optional] TensorFlow master URL.")
+
+flags.DEFINE_integer(
+    "num_tpu_cores", 8,
+    "Only used if `use_tpu` is True. Total number of TPU cores to use.")
 
 flags.DEFINE_bool(
     "verbose_logging", False,
@@ -119,6 +153,12 @@ flags.DEFINE_float(
     "null_score_diff_threshold", 0.0,
     "If null_score - best_non_null is greater than the threshold predict null.")
 
+
+class SquadExample(object):
+  """A single training/test example for simple sequence classification.
+
+     For examples without an answer, the start and end position are -1.
+  """
 
   def __init__(self,
                qas_id,
---
+++ bert_yo/utils.py
@@ -0,0 +1,25 @@
+def validate_flags_or_throw(FLAGS, bert_config):
+    """Validate the input FLAGS or throw an exception."""
+    if not FLAGS.do_train and not FLAGS.do_predict:
+        raise ValueError(
+            "At least one of `do_train` or `do_predict` must be True.")
+
+    if FLAGS.do_train:
+        if not FLAGS.train_file:
+            raise ValueError(
+                "If `do_train` is True, then `train_file` must be specified.")
+    if FLAGS.do_predict:
+        if not FLAGS.predict_file:
+            raise ValueError(
+                "If `do_predict` is True, then `predict_file` must be specified.")
+
+    if FLAGS.max_seq_length > bert_config.max_position_embeddings:
+        raise ValueError(
+            "Cannot use sequence length %d because the BERT model "
+            "was only trained up to sequence length %d" %
+            (FLAGS.max_seq_length, bert_config.max_position_embeddings))
+
+    if FLAGS.max_seq_length <= FLAGS.max_query_length + 3:
+        raise ValueError(
+            "The max_seq_length (%d) must be greater than max_query_length "
+            "(%d) + 3" % (FLAGS.max_seq_length, FLAGS.max_query_length))
