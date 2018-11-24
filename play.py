fix return calculation

play.py
@@ -12,16 +12,16 @@ import random
 # https://github.com/yaserkl/RLSeq2Seq/blob/7e019e8e8c006f464fdc09e77169680425e83ad1/src/model.py#L348
 
 EPISODES = 10000000
-BATCH_SIZE = 128
+BATCH_SIZE = 64
 # MODEL_BATCH_SIZE = 1
-GAMMA = 0.7  # TODO
+GAMMA = 1  # TODO
 USE_GLOVE = False
 if USE_GLOVE:
     # 1024 if using glove
     EMBEDDING_DIM = 100
 else:
     # 256 if without pretrained embedding
-    EMBEDDING_DIM = 5
+    EMBEDDING_DIM = 16
 
 MAX_TARGET_LEN = 20  # TODO: hack
 UNITS = 128
@@ -96,45 +96,23 @@ def main():
             # record history (to be used for gradient updating after the episode is done)
         # End of Episode
         # Update policy
-        if episode > 0 and (episode + 1) % (BATCH_SIZE / CONVO_LEN * 2) == 0:
+        if len(history) >= BATCH_SIZE:
+
+            def get_returns(r: float, seq_len: int):
+                return list(reversed([
+                    r * (GAMMA ** t) for t in range(seq_len)
+                ]))
+
+            batch = history[:BATCH_SIZE]
             print("==========================")
             print("Episode # ", episode)
             print("Samples from episode with rewards > 0: ")
-
-            good_rewards = [(s, a, r) for s, a, r in history if r > 0]
+            good_rewards = [(s, a, r) for s, a, r in batch if r > 0]
             for s, a, r in random.sample(good_rewards, min(len(good_rewards), 5)):
                 print("prev_state: ", s)
                 print("action: ", a)
                 print("reward: ", r)
-
-            # TODO: this reward accumulation comes from the cartpole example.
-            # may not be correct for our purpose.
-            running_add = 0
-
-            acc_reward_b = []
-
-            for _, _, curr_reward in reversed(history):
-                if curr_reward == 0:
-                    running_add = 0
-                else:
-                    running_add = running_add * GAMMA + curr_reward
-                acc_reward_b.append(running_add)
-            acc_reward_b = list(reversed(acc_reward_b))
-
-            # normalize reward
-            reward_mean = np.mean(acc_reward_b)
-            reward_std = np.std(acc_reward_b)
-            norm_reward_b = [(r - reward_mean) /
-                             reward_std for r in acc_reward_b]
-            print(
-                "all rewards: min=%f, max=%f, median=%f" %
-                (np.min(acc_reward_b), np.max(acc_reward_b), np.median(acc_reward_b))
-            )
-            print("avg reward: ", sum(
-                acc_reward_b) / len(acc_reward_b))
-
-            loss = 0
-            loss_bl = 0
+                # print("return: ", get_returns(r, MAX_TARGET_LEN))
 
             def sentence_to_idxs(sentence: str):
                 return [env.lang.word2idx[token]
@@ -156,37 +134,66 @@ def main():
                     padding='post'
                 )
 
+            state_inp_b, action_encs_b, reward_b, ret_seq_b = zip(*[
+                [
+                    sentence_to_idxs(state),
+                    action_to_encs(action),
+                    reward,
+                    get_returns(reward, MAX_TARGET_LEN)
+                ]
+                for state, action, reward in batch
+            ])
+            action_encs_b = maybe_pad_sentence(action_encs_b)
+            action_encs_b = tf.expand_dims(
+                tf.convert_to_tensor(action_encs_b), -1)
+
+            reward_mean = np.mean(ret_seq_b)
+            reward_std = np.std(ret_seq_b)
+            ret_seq_b = [(r - reward_mean) / reward_std for r in ret_seq_b]
+
+            ret_seq_b = tf.cast(tf.convert_to_tensor(ret_seq_b), 'float32')
+
+            print(
+                "all returns: min=%f, max=%f, median=%f" %
+                (np.min(ret_seq_b), np.max(ret_seq_b), np.median(ret_seq_b))
+            )
+            print("avg reward: ", sum(reward_b) / len(reward_b))
+
+            loss = 0
+            loss_bl = 0
+
             with tf.GradientTape() as l_tape, tf.GradientTape() as bl_tape:
                 # accumulate gradient with GradientTape
                 init_hidden_b = initialize_hidden_state(BATCH_SIZE, UNITS)
 
-                state_inp_b, action_encs_b = zip(*[
-                    [sentence_to_idxs(state), action_to_encs(action)]
-                    for (state, action, _), norm_reward in zip(history, norm_reward_b)
-                ])
                 state_inp_b = maybe_pad_sentence(state_inp_b)
                 state_inp_b = tf.convert_to_tensor(state_inp_b)
 
-                bl_val = baseline(tf.cast(state_inp_b, 'float32'))
-                norm_reward_b = tf.cast(
-                    tf.convert_to_tensor(norm_reward_b), 'float32')
-                norm_reward_b -= bl_val
-
-                action_encs_b = maybe_pad_sentence(action_encs_b)
-                action_encs_b = tf.expand_dims(
-                    tf.convert_to_tensor(action_encs_b), -1)
                 enc_hidden_b = encoder(state_inp_b, init_hidden_b)
                 dec_hidden_b = enc_hidden_b
                 max_sentence_len = action_encs_b.shape[1]
+                prev_w_idx_b = tf.expand_dims(
+                    tf.cast(
+                        tf.convert_to_tensor(
+                            [env.lang.word2idx[BEGIN_TAG]] * BATCH_SIZE),
+                        'float32'
+                    ), -1
+                )
                 for i in range(max_sentence_len):
-                    curr_w_idx_b = action_encs_b[:, i]
+
+                    bl_val_b = baseline(tf.cast(dec_hidden_b, 'float32'))
+                    ret_b = ret_seq_b[:, i]
+                    delta_b = ret_b - bl_val_b
+
                     w_probs_b, dec_hidden_b = decoder(
-                        curr_w_idx_b, dec_hidden_b)
-                    # w_probs = tf.nn.softmax(w_probs)
+                        prev_w_idx_b, dec_hidden_b
+                    )
+                    curr_w_idx_b = action_encs_b[:, i]
                     dist = tf.distributions.Categorical(w_probs_b)
-                    # TODO: check formulation
-                    loss += - dist.log_prob(curr_w_idx_b) * norm_reward_b
-                    loss_bl += norm_reward_b
+                    loss += dist.log_prob(curr_w_idx_b) * ret_b
+                    loss_bl += ret_b * delta_b
+
+                    prev_w_idx_b = curr_w_idx_b
 
             # calculate cumulative gradients
             model_vars = encoder.variables + decoder.variables
@@ -200,7 +207,7 @@ def main():
             bl_optimizer.apply_gradients(zip(grads_bl, baseline.variables))
 
             # Reset everything for the next episode
-            history = []
+            history = history[BATCH_SIZE:]
 
 
 main()